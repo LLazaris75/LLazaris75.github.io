86a384db3dd260b82b3e62fd5ce7a6f6c15b2379
Luca Lazaris <luca.lazaris@GCCYJPP0J1WK.hitronhub.home>
2020-03-25 09:09:27 -0400

    Final Copy

---
+++ feb25listen.js
@@ -0,0 +1,11 @@
+console.log("Feb 25")
+var countries = ["Canada, India, China, Bahamas"]
+var population = [36, 1300, 1380, 0.4]
+
+
+document.getElementById("clickMe").onclick = clicked;
+
+function clicked() {
+	var value = document.getElementById("fname").value
+	console.log(value);
+}
